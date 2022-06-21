2cbee237c0404b7072aa8143bc70b37b4e135ad8
dangcaptkd2 <thinhquyen9461@gmail.com>
2022-06-21 08:21:18 +0700

    stop crypto, weapon

--- nsfw/image_nsfw.py
+++ nsfw/image_nsfw.py
@@ -57,13 +57,15 @@ def upload_check_ocr_url():
 		result["time_detect_flag_in"] = round(time.time()-start, 5)
 
 		start = time.time()
-		result_weapon = detect_weapon(img_path, draw=True)
-		result['weapon'] = result_weapon
+		#result_weapon = detect_weapon(img_path, draw=True)
+		# result['weapon'] = result_weapon
+		result['weapon'] = False
 		result["time_detect_weapon_in"] = round(time.time()-start, 5)
 
 		start = time.time()
-		result_crypto = detect_crypto(img_path, draw=True)
-		result['crypto'] = result_crypto
+		# result_crypto = detect_crypto(img_path, draw=True)
+		# result['crypto'] = result_crypto
+		result['crypto'] = False
 		result["time_detect_crypto_in"] = round(time.time()-start, 5)
 		return result
 	return render_template("upload.html")
